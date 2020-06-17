b40dad82de081c4cbd39e3b5554344969f578cd2
guy <guy.selenium@gmail.com>
2020-06-17 21:34:41 +0300

    Updates

---
+++ precache-manifest.06702c225d01affd73493732173b4f61.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "531f8a67ec528175a4cb38e7338fdc1e",
+    "url": "/Weather-app/index.html"
+  },
+  {
+    "revision": "e5b7063950b927ee28c7",
+    "url": "/Weather-app/static/js/2.87795531.chunk.js"
+  },
+  {
+    "revision": "d2966845b94a3318bf32eecc7af8015d",
+    "url": "/Weather-app/static/js/2.87795531.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e084bdb7fb633010b2b8",
+    "url": "/Weather-app/static/js/main.ffdf166b.chunk.js"
+  },
+  {
+    "revision": "9d2b0e576db66cd702e0",
+    "url": "/Weather-app/static/js/runtime-main.6d0542b5.js"
+  },
+  {
+    "revision": "246c1201edbfbe27c6efc421dbc7eec5",
+    "url": "/Weather-app/static/media/appBackground.246c1201.jpg"
+  }
+]);
